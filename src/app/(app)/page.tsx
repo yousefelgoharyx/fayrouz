1add64aa0050819e3d2cfc1df80555e5b62577fe
Yousef <yousefelgoharyx@gmail.com>
2023-06-28 15:04:57 +0300

    Hopefully fixes vercel

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -5,9 +5,9 @@ export default function Home() {
     <div>
       <h1 className="mb-4 text-2xl font-bold">Today&apos;s Selection</h1>
       <Suspense fallback={<div>Loading...</div>}>
-        {/* @ts-expect-error Server Component */}
         <TodaysSelection />
       </Suspense>
     </div>
   );
 }
+
